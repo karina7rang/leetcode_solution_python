94c42719c4b4a9406a4b2b65de0b26e434196ae8
karina <karina7rang@gmail.com>
2022-02-27 23:45:12 -0500

    Create easy_template.py

---
+++ easy_template.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/xxx/
+
+class Solution1:
+    def xxx(self):
+
+class Solution2:
+    def xxx(self):
+
+test_cases = [
+    
+]
+
+for i in test_cases:
+    res = Solution1().xxx(i)
+    print(res)
+print('------------------')
+for i in test_cases:
+    res = Solution2().xxx(i)
+    print(res)
