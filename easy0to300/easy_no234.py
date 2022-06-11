b04cf00a90f0e6988585e91c716b3feb8157d339
karina <karina7rang@gmail.com>
2022-06-11 18:30:18 -0400

    Update easy_no234.py

--- easy0to300/easy_no234.py
+++ easy0to300/easy_no234.py
@@ -32,3 +32,21 @@ class Solution:
             if sum(j)!=((len(j)/2)*length):
                 return False
         return True
+
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        rev = None
+        slow = fast = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+        if fast:
+            slow = slow.next
+
+        while rev and rev.val==slow.val:
+            rev = rev.next
+            slow = slow.next
+
+        return not rev
